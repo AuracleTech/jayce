d01a1584bb490d6c4443cd5bbe30b455e7287458
AuracleTech <VirgilFabre@outlook.com>
2023-08-08 01:36:36 -0400

    Add generic trait to experiments

--- src/internal.rs
+++ src/internal.rs
@@ -1,39 +1,77 @@
 use super::regexify;
 use regex::Regex;
 
-lazy_static::lazy_static! (
-pub static ref DUOS_RUST: Vec<(&'static str, Regex)> = vec![
-    (
-        "keyword",
-        regexify!(
-            r"^(mut|let|if|else|fn|struct|enum|match|use|mod|pub|crate|impl|trait|for|while|loop|break|continue|return|as|const|static|type|where|unsafe|extern|ref|self|super|in|move|dyn|abstract|async|await|become|box|do|final|macro|override|priv|typeof|unsized|virtual|yield)\b"
+#[derive(Debug, Clone, PartialEq)]
+pub enum Duos {
+    Keyword,
+    String,
+    Char,
+    Lifetime,
+    Operator,
+    Identifier,
+    Integer,
+    Float,
+    DoubleColon,
+    Semicolon,
+    OpenBrace,
+    CloseBrace,
+    OpenParen,
+    CloseParen,
+    OpenBracket,
+    CloseBracket,
+    Comma,
+    Hash,
+    Dot,
+    Colon,
+    Pipe,
+    OpenAngle,
+    CloseAngle,
+    Caret,
+    TempBorrow,
+    Question,
+    MacroExclamation,
+}
+
+fn generic() -> Vec<(Duos, Regex)> {
+    vec![
+        (
+            Duos::Keyword,
+            regexify!(
+                r"^(mut|let|if|else|fn|struct|enum|match|use|mod|pub|crate|impl|trait|for|while|loop|break|continue|return|as|const|static|type|where|unsafe|extern|ref|self|super|in|move|dyn|abstract|async|await|become|box|do|final|macro|override|priv|typeof|unsized|virtual|yield)\b"
+            ),
         ),
-    ),
-    ("string", regexify!(r#"^"[^"]*""#)),
-    ("char", regexify!(r"^'(.|\\n)'")),
-    ("lifetime", regexify!(r"^'(?:[a-z_][a-z0-9_]*|static)\b")),
-    ("operator", regexify!(r"^(=|\+|-|\*|/|%)")),
-    ("identifier", regexify!(r"^[a-zA-Z_][a-zA-Z0-9_]*")),
-    ("integer", regexify!(r"^\d+")),
-    ("float", regexify!(r"^\d+\.\d+")),
-    ("double_colon", regexify!(r"^::")),
-    ("semicolon", regexify!(r"^;")),
-    ("open_brace", regexify!(r"^\{")),
-    ("close_brace", regexify!(r"^\}")),
-    ("open_paren", regexify!(r"^\(")),
-    ("close_paren", regexify!(r"^\)")),
-    ("open_bracket", regexify!(r"^\[")),
-    ("close_bracket", regexify!(r"^\]")),
-    ("comma", regexify!(r"^,")),
-    ("hash", regexify!(r"^#")),
-    ("dot", regexify!(r"^\.")),
-    ("colon", regexify!(r"^:")),
-    ("pipe", regexify!(r"^\|")),
-    ("open_angle", regexify!(r"^<")),
-    ("close_angle", regexify!(r"^>")),
-    ("caret", regexify!(r"^\^")),
-    ("temp_borrow", regexify!(r"^&")),
-    ("question", regexify!(r"^\?")),
-    ("macro_exclamation", regexify!(r"^!")),
-];
+        (Duos::String, regexify!(r#"^"[^"]*""#)),
+        (Duos::Char, regexify!(r"^'(.|\\n)'")),
+        (
+            Duos::Lifetime,
+            regexify!(r"^'(?:[a-z_][a-z0-9_]*|static)\b"),
+        ),
+        (Duos::Operator, regexify!(r"^(=|\+|-|\*|/|%)")),
+        (Duos::Identifier, regexify!(r"^[a-zA-Z_][a-zA-Z0-9_]*")),
+        (Duos::Integer, regexify!(r"^\d+")),
+        (Duos::Float, regexify!(r"^\d+\.\d+")),
+        (Duos::DoubleColon, regexify!(r"^::")),
+        (Duos::Semicolon, regexify!(r"^;")),
+        (Duos::OpenBrace, regexify!(r"^\{")),
+        (Duos::CloseBrace, regexify!(r"^\}")),
+        (Duos::OpenParen, regexify!(r"^\(")),
+        (Duos::CloseParen, regexify!(r"^\)")),
+        (Duos::OpenBracket, regexify!(r"^\[")),
+        (Duos::CloseBracket, regexify!(r"^\]")),
+        (Duos::Comma, regexify!(r"^,")),
+        (Duos::Hash, regexify!(r"^#")),
+        (Duos::Dot, regexify!(r"^\.")),
+        (Duos::Colon, regexify!(r"^:")),
+        (Duos::Pipe, regexify!(r"^\|")),
+        (Duos::OpenAngle, regexify!(r"^<")),
+        (Duos::CloseAngle, regexify!(r"^>")),
+        (Duos::Caret, regexify!(r"^\^")),
+        (Duos::TempBorrow, regexify!(r"^&")),
+        (Duos::Question, regexify!(r"^\?")),
+        (Duos::MacroExclamation, regexify!(r"^!")),
+    ]
+}
+
+lazy_static::lazy_static! (
+pub static ref DUOS_RUST: Vec<(Duos, Regex)> = generic();
 );
--- tests/experiment.rs
+++ tests/experiment.rs
@@ -1,4 +1,7 @@
-use jayce::{internal::DUOS_RUST, regexify, Tokenizer};
+use jayce::{
+    internal::{Duos, DUOS_RUST},
+    regexify, Tokenizer,
+};
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -72,27 +75,27 @@ let f = d + e;
 /**/
 
 "#;
-const EXPECTED_COMMENTS: &[(&str, &str, (usize, usize))] = &[
-    ("keyword", "let", (2, 1)),
-    ("identifier", "a", (2, 5)),
-    ("operator", "=", (2, 7)),
-    ("integer", "5", (2, 9)),
-    ("semicolon", ";", (2, 10)),
-    ("keyword", "let", (3, 1)),
-    ("identifier", "b", (3, 5)),
-    ("semicolon", ";", (3, 6)),
-    ("keyword", "let", (6, 1)),
-    ("identifier", "d", (6, 5)),
-    ("operator", "=", (6, 7)),
-    ("integer", "5", (6, 9)),
-    ("semicolon", ";", (6, 10)),
-    ("keyword", "let", (7, 1)),
-    ("identifier", "f", (7, 5)),
-    ("operator", "=", (7, 7)),
-    ("identifier", "d", (7, 9)),
-    ("operator", "+", (7, 11)),
-    ("identifier", "e", (7, 13)),
-    ("semicolon", ";", (7, 14)),
+const EXPECTED_COMMENTS: &[(Duos, &str, (usize, usize))] = &[
+    (Duos::Keyword, "let", (2, 1)),
+    (Duos::Identifier, "a", (2, 5)),
+    (Duos::Operator, "=", (2, 7)),
+    (Duos::Integer, "5", (2, 9)),
+    (Duos::Semicolon, ";", (2, 10)),
+    (Duos::Keyword, "let", (3, 1)),
+    (Duos::Identifier, "b", (3, 5)),
+    (Duos::Semicolon, ";", (3, 6)),
+    (Duos::Keyword, "let", (6, 1)),
+    (Duos::Identifier, "d", (6, 5)),
+    (Duos::Operator, "=", (6, 7)),
+    (Duos::Integer, "5", (6, 9)),
+    (Duos::Semicolon, ";", (6, 10)),
+    (Duos::Keyword, "let", (7, 1)),
+    (Duos::Identifier, "f", (7, 5)),
+    (Duos::Operator, "=", (7, 7)),
+    (Duos::Identifier, "d", (7, 9)),
+    (Duos::Operator, "+", (7, 11)),
+    (Duos::Identifier, "e", (7, 13)),
+    (Duos::Semicolon, ";", (7, 14)),
 ];
 
 #[test]
@@ -122,11 +125,10 @@ fn unexpected() {
 
 // Verify function
 
-fn verify(
-    source: &str,
-    duos: &'static [(&'static str, regex::Regex)],
-    expected: &[(&str, &str, (usize, usize))],
-) {
+fn verify<T>(source: &str, duos: &'static [(T, Regex)], expected: &[(T, &str, (usize, usize))])
+where
+    T: PartialEq + std::fmt::Debug,
+{
     let mut tokenizer = Tokenizer::new(source, duos.into());
 
     for (kind, value, (line, column)) in expected {
